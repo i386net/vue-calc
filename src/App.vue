4fa1d434a4023f8d5f6525c94ec2794c6226837e
DK <13776265+i386net@users.noreply.github.com>
2020-07-11 13:17:32 +0300

    add page style

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: darkorange;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
