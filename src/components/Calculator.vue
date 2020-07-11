693278da23e799e26b9be217c0284279ab6ebc1e
DK <13776265+i386net@users.noreply.github.com>
2020-07-11 07:35:52 +0300

    added classes and style

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="calculator">
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
-    <div>hello</div>
-
+    <div class="calculator__display">11111111111</div>
+    <div class="calculator__button calculator__button_special">AC</div>
+    <div class="calculator__button calculator__button_special">+/-</div>
+    <div class="calculator__button calculator__button_special">%</div>
+    <div class="calculator__button calculator__button_operator">÷</div>
+    <div class="calculator__button calculator__button_number">7</div>
+    <div class="calculator__button calculator__button_number">8</div>
+    <div class="calculator__button calculator__button_number">9</div>
+    <div class="calculator__button calculator__button_operator">×</div>
+    <div class="calculator__button calculator__button_number">4</div>
+    <div class="calculator__button calculator__button_number">5</div>
+    <div class="calculator__button calculator__button_number">6</div>
+    <div class="calculator__button calculator__button_operator">−</div>
+    <div class="calculator__button calculator__button_number">1</div>
+    <div class="calculator__button calculator__button_number">2</div>
+    <div class="calculator__button calculator__button_number">3</div>
+    <div class="calculator__button calculator__button_operator">+</div>
+    <div class="calculator__button zero calculator__button_number">0</div>
+    <div class="calculator__button calculator__button_number">,</div>
+    <div class="calculator__button calculator__button_operator">=</div>
   </div>
 </template>
 
@@ -23,5 +35,26 @@ export default {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(50px, auto);
+    font-size: 40px;
+    color: #ffffff;
+  }
+  .calculator__display {
+    grid-column: 1/5;
+    background-color: rgb(88, 92, 95);
+  }
+  .zero {
+    grid-column: 1 / 3;
+  }
+  .calculator__button {
+    border: 1px solid rgb(88, 92, 95);
+  }
+  .calculator__button_operator {
+    background-color: darkorange;
+  }
+  .calculator__button_number {
+    background-color: rgb(129, 131, 133);
+  }
+  .calculator__button_special {
+    background-color: rgb(104, 107, 110);
   }
 </style>
